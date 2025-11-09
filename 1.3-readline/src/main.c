375fcf6652bf938054b9292bf323bdd653d869be
Muhammad Muneeb Shafiq <muneebshafique298@gmail.com>
2025-11-09 10:47:27 -0800

    Add 1.3-readline: GNU Readline integration with tab completion

---
+++ 1.3-readline/src/main.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+extern char* history[HISTORY_SIZE];
+extern int history_count;
+
+void add_to_history(char* cmd) {
+    if (history_count < HISTORY_SIZE) {
+        history[history_count] = strdup(cmd);
+        history_count++;
+    } else {
+        free(history[0]);
+        for (int i = 1; i < HISTORY_SIZE; i++) {
+            history[i-1] = history[i];
+        }
+        history[HISTORY_SIZE-1] = strdup(cmd);
+    }
+}
+
+int main() {
+    char* cmdline;
+    char** arglist;
+
+    rl_bind_key('\t', rl_complete);
+
+    while (1) {
+
+        cmdline = readline(PROMPT);
+        if (cmdline == NULL) { 
+            printf("\nExiting shell...\n");
+            break;
+        }
+
+        if (strlen(cmdline) == 0) { 
+            free(cmdline);
+            continue;
+        }
+
+
+        if (cmdline[0] == '!') {
+            int index = atoi(cmdline + 1);
+            if (index > 0 && index <= history_count) {
+                free(cmdline);
+                cmdline = strdup(history[index - 1]);
+                printf("%s%s\n", PROMPT, cmdline);
+            } else {
+                printf("Invalid history reference.\n");
+                free(cmdline);
+                continue;
+            }
+        }
+
+        add_history(cmdline);
+
+        add_to_history(cmdline);
+
+        arglist = tokenize(cmdline);
+        if (arglist != NULL) {
+            if (!handle_builtin(arglist)) {
+                execute(arglist); 
+            }
+
+            for (int i = 0; arglist[i] != NULL; i++) {
+                free(arglist[i]);
+            }
+            free(arglist);
+        }
+
+        free(cmdline);
+    }
+
+    printf("\nShell exited.\n");
+    return 0;
+}
---
+++ 1.3-readline/src/shell.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+#include <unistd.h>
+
+
+char* history[HISTORY_SIZE];
+int history_count = 0;
+
+char* read_cmd(char* prompt, FILE* fp) {
+    printf("%s", prompt);
+    char* cmdline = (char*) malloc(sizeof(char) * MAX_LEN);
+    int c, pos = 0;
+
+    while ((c = getc(fp)) != EOF) {
+        if (c == '\n') break;
+        cmdline[pos++] = c;
+    }
+
+    if (c == EOF && pos == 0) {
+        free(cmdline);
+        return NULL; 
+    }
+    
+    cmdline[pos] = '\0';
+    return cmdline;
+}
+
+char** tokenize(char* cmdline) {
+    if (cmdline == NULL || cmdline[0] == '\0' || cmdline[0] == '\n') {
+        return NULL;
+    }
+
+    char** arglist = (char**)malloc(sizeof(char*) * (MAXARGS + 1));
+    for (int i = 0; i < MAXARGS + 1; i++) {
+        arglist[i] = (char*)malloc(sizeof(char) * ARGLEN);
+        bzero(arglist[i], ARGLEN);
+    }
+
+    char* cp = cmdline;
+    char* start;
+    int len;
+    int argnum = 0;
+
+    while (*cp != '\0' && argnum < MAXARGS) {
+        while (*cp == ' ' || *cp == '\t') cp++; 
+        
+        if (*cp == '\0') break; 
+
+        start = cp;
+        len = 1;
+        while (*++cp != '\0' && !(*cp == ' ' || *cp == '\t')) {
+            len++;
+        }
+        strncpy(arglist[argnum], start, len);
+        arglist[argnum][len] = '\0';
+        argnum++;
+    }
+
+    if (argnum == 0) { 
+        for(int i = 0; i < MAXARGS + 1; i++) free(arglist[i]);
+        free(arglist);
+        return NULL;
+    }
+
+    arglist[argnum] = NULL;
+    return arglist;
+}
+
+
+
+int handle_builtin(char** arglist) {
+    if (arglist[0] == NULL) return 0;
+
+    // exit
+    if (strcmp(arglist[0], "exit") == 0) {
+        printf("Exiting shell...\n");
+        exit(0);
+    }
+
+    // cd
+    if (strcmp(arglist[0], "cd") == 0) {
+        if (arglist[1] == NULL) {
+            fprintf(stderr, "cd: missing argument\n");
+        } else {
+            if (chdir(arglist[1]) != 0) {
+                perror("cd failed");
+            }
+        }
+        return 1;
+    }
+
+    // help
+    if (strcmp(arglist[0], "help") == 0) {
+        printf("Built-in commands:\n");
+        printf("  cd <directory>  : Change directory.\n");
+        printf("  exit            : Exit the shell.\n");
+        printf("  help            : Show help.\n");
+        printf("  jobs            : Show job placeholder.\n");
+        return 1;
+    }
+
+    // jobs 
+    if (strcmp(arglist[0], "jobs") == 0) {
+        printf("Job control not yet implemented.\n");
+        return 1;
+    }
+
+	// history
+    if (strcmp(arglist[0], "history") == 0) {
+        for (int i = 0; i < history_count; i++) {
+        printf("%d  %s\n", i+1, history[i]);
+    }
+    return 1;
+}
+
+
+    return 0; 
+}
