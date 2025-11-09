10b66b5198179ec430fa6600f1c4e1712ca25b0f
Muhammad Muneeb Shafiq <muneebshafique298@gmail.com>
2025-11-09 10:45:22 -0800

    Add 1.0-base: Basic shell implementation with command parsing and execution

---
+++ 1.0-base/src/main.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+int main()
+{
+    char *cmdline;
+    char **arglist;
+
+    while ((cmdline = read_cmd(PROMPT, stdin)) != NULL)
+    {
+        if ((arglist = tokenize(cmdline)) != NULL)
+        {
+            execute(arglist);
+            for (int i = 0; arglist[i] != NULL; i++)
+            {
+                free(arglist[i]);
+            }
+            free(arglist);
+        }
+        free(cmdline);
+    }
+
+    printf("\nShell exited.\n");
+    return 0;
+}
